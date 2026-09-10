95b2e9156d9cb43ee4cdb8712d245ce66bb06677
nquadri2020 <nquadri2020@fau.edu>
2026-09-09 20:27:49 -0400

    Update p1_Quadri_Naabeghah.py
    drafted while loop for getting inputs and defining b^2-4ac. Fixed a local issue with conflicting python versions that was causing an error with importing matplotlib to my pc for the right version of python

--- Homework1/p1_Quadri_Naabeghah.py
+++ Homework1/p1_Quadri_Naabeghah.py
@@ -1,1 +1,39 @@
 import matplotlib.pyplot as plt
+
+while True:
+    a_input = input("Enter a: ")
+    if a_input == "":
+        break
+    a = float(a_input)
+    b = float(input("Enter b: "))
+    c = float(input("Enter c: "))
+    sqrt = b ** 2 - 4 * a * c
+
+    if sqrt < 0:
+        print("no real solutions")
+        xopt = -b / (2 * a)
+        x_min = xopt - 5
+        x_max = xopt + 5
+
+    elif sqrt == 0:
+        x1 = -b / (2 * a)
+        print("one solution: {:.5f}".format(x1))
+        x_min = x1 - 5
+        x_max = x1 + 5
+
+    else:
+        x1 = (-b - sqrt ** 0.5) / (2 * a)
+        x2 = (-b + sqrt ** 0.5) / (2 * a)
+        print("two solutions: x1={:.5f} x2={:.5f}".format(x1, x2))
+        margin = 2
+        x_min = min(x1, x2) - margin
+        x_max = max(x1, x2) + margin
+
+    xs = [
+        x_min + i * (x_max - x_min) / 149
+        for i in range(150)
+    ]
+    ys = [
+        a * x ** 2 + b * x + c
+        for x in xs
+    ]
